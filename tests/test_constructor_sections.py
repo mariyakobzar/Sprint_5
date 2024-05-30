e794dd751c0293c21cc8896cb38ebe6f1f32b890
Maria Kobzar <kobzarms@gmail.com>
2024-05-30 18:46:43 +0200

    Project_Sprint_5

--- tests/test_constructor_sections.py
+++ tests/test_constructor_sections.py
@@ -1,5 +1,6 @@
 import time
-
+from helpers import Helpers
+from locators import TestLocators
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from locators import TestLocators
@@ -8,6 +9,32 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 class TestConstructionSections():
+
+    def test_constructor_entrance(self, driver):
+        email = Helpers.create_random_email()
+        password = str(Helpers.create_random_password())
+        #options = Options()
+        #options.add_argument('--window-size=1920,1080')
+        #driver = webdriver.Chrome(options=options)
+        #driver.get("https://stellarburgers.nomoreparties.site/")
+        driver.find_element(*TestLocators.SEARCH_ACCOUNT).click()
+        driver.find_element(*TestLocators.SEARCH_REGISTRATION).click()
+        driver.find_element(*TestLocators.SEARCH_NAME).send_keys('Maria')
+        driver.find_element(*TestLocators.SEARCH_EMAIL).send_keys(email)
+        driver.find_element(*TestLocators.SEARCH_PASSWORD).send_keys(password)
+        driver.find_element(*TestLocators.SEARCH_BUTTON_REGISTER).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.presence_of_element_located(TestLocators.SEARCH_HEADER_ENTRANCE))
+        driver.find_element(*TestLocators.SEARCH_EMAIL_ENTRANCE).send_keys(email)
+        driver.find_element(*TestLocators.SEARCH_PASSWORD_ENTRANCE).send_keys(password)
+        driver.find_element(*TestLocators.SEARCH_ENTER).click()
+        driver.find_element(*TestLocators.SEARCH_LOGO).click()
+        driver.find_element(*TestLocators.SEARCH_ACCOUNT).click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.presence_of_element_located(TestLocators.SEARCH_PROFILE))
+        driver.find_element(*TestLocators.SEARCH_CONSTRUCTOR).click()
+        assert driver.find_element(*TestLocators.SEARCH_HEADER_CONSTRUCTOR).text == "Соберите бургер"
+        #driver.quit()
 
     def test_constructor_sections_ingredients(self, driver):
         #options = Options()
